64c8775ff02991b5970b39055322f93ea4032390
Satish H <satish.h@target.com>
2016-10-24 16:14:51 +0530

    css fixes done due to alignment issues on TTC server

--- wp-config.php
+++ wp-config.php
@@ -20,14 +20,14 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
+//define('DB_NAME', 'tgt_dam_opentext');
 define('DB_NAME', 'tgt_dam_opentext');
-
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'Target@1234');
-
+//define('DB_PASSWORD', '');
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
 
